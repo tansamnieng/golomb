648ae752f15ba42d96f2267f1076e9efa9db0744
vmatter <43481916+vmatter@users.noreply.github.com>
2020-09-25 11:32:01 -0300

    Codificador, decodificador e arquivos de teste
    Todo o projeto criado na máquina foi adicionado no git.

---
+++ main.py
@@ -0,0 +1,55 @@
+from codificador import Codificador
+from decodificador import Decodificador
+
+##### Configuracao do codificador #####
+
+# Se deseja codificar:
+codificar = True
+
+# Nome do arquivo:
+nome_arquivo = 'alice29.txt'
+
+''' Tipos
+    0 = Golomb
+    1 = Elias Gamma
+    2 = Fibonacci
+    3 = Unaria
+    4 = Delta
+'''
+
+# Escolha o tipo de codificacao
+tipo = 4
+
+golomb_divisor = 16 # Deve ser potencia de 2 (2, 4, 8, 16,...)
+
+#######################################
+
+##### Configuracao do decodificador #####
+
+# Se deseja decodificar:
+decodificar = True
+
+'''Extensoes dos arquivos possiveis:
+    .golomb     = Golomb
+    .eliasgamma = Elias Gamma
+    .fibonacci  = Fibonacci
+    .unaria     = Unaria
+    .delta      = Delta
+'''
+# Nome do arquivo que deseja decodificar:
+nome_arquivo_codificado = 'alice29.delta'
+
+# Extensao do novo arquivo decodificado
+extensao_saida = 'txt2'
+
+#########################################
+
+# Codificacao:
+if codificar:
+    codifica = Codificador()
+    codifica.codificar(nome_arquivo, tipo, golomb_divisor)
+
+# Decodificacao:
+if decodificar:
+    decodifica = Decodificador()
+    decodifica.decodificar(nome_arquivo_codificado, extensao_saida)
